6f853e6515a5669683c6f9d7338cbd496ee2f3a5
temppao <145994548+temppao@users.noreply.github.com>
2023-11-16 20:27:20 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,19 +2,14 @@ import streamlit as st
 import openai
 import os
 
-# Set your OpenAI API key
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-# Set prompt
 prompt = "\n\nYou must classify the statement above whether it is hate speech or not. If the statement is not considered hate speech, you must output 'This is not an example of hate speech.'; however, if the example is considered hate speech, you must specify what kind of hate speech it is (examples: racism, sexism, religion, etc.), and you must output 'This is an example of hate speech. It is (insert type of hate speech).'"
 
-# Streamlit app title
-st.title("Hate Speech Classification with GPT-3.5 Turbo")
+st.title("Hate Speech Classification")
 
-# Streamlit input for user text
 user_input = st.text_area("Enter text for hate speech classification:")
 
-# Function to classify hate speech using GPT-3.5 Turbo
 def classify_hate_speech(text):
     # Replace "your_engine_id" with the ID of the GPT-3.5 Turbo engine
     response = openai.Completion.create(
@@ -22,19 +17,13 @@ def classify_hate_speech(text):
         prompt=text,
         n=1,
     )
-    
-    # Get the generated text from the response
     generated_text = response['choices'][0]['text']
-    
     return generated_text
 
-# Check if the user has entered text
 if user_input:
     text = user_input.join(prompt)
-    # Classify hate speech using GPT-3.5 Turbo
     classification_result = classify_hate_speech(text)
-    
-    # Display the results
+
     st.subheader("Classification Result:")
     st.write(classification_result)
 else:
