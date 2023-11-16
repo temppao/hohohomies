85a72b8d37c8a8ab805dc66fb5c8f6cb28122c51
temppao <145994548+temppao@users.noreply.github.com>
2023-11-16 16:42:22 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -6,7 +6,7 @@ import os
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 # Set prompt
-prompt = "You must classify the statement above whether it is hate speech or not. If the statement is not considered hate speech, you must output 'This is not an example of hate speech.'; however, if the example is considered hate speech, you must specify what kind of hate speech it is (examples: racism, sexism, religion, etc.), and you must output 'This is an example of hate speech. It is (insert type of hate speech).'"
+prompt = "\n\nYou must classify the statement above whether it is hate speech or not. If the statement is not considered hate speech, you must output 'This is not an example of hate speech.'; however, if the example is considered hate speech, you must specify what kind of hate speech it is (examples: racism, sexism, religion, etc.), and you must output 'This is an example of hate speech. It is (insert type of hate speech).'"
 
 # Streamlit app title
 st.title("Hate Speech Classification with GPT-3.5 Turbo")
@@ -18,11 +18,9 @@ user_input = st.text_area("Enter text for hate speech classification:")
 def classify_hate_speech(text):
     # Replace "your_engine_id" with the ID of the GPT-3.5 Turbo engine
     response = openai.Completion.create(
-        engine="text-davinci-003",  # Use GPT-3.5 Turbo engine
+        engine="gpt-3.5-turbo",
         prompt=text,
-        max_tokens=100,
         n=1,
-        stop=None
     )
     
     # Get the generated text from the response
